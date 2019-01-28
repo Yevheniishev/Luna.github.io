f7a03cb295d2918632914319d4a45f33d92d43f6
Yevhenii <yevhenii.shevchenko@ukr.net>
2019-01-28 14:42:48 +0200

    28.01

--- dist/js/anchor.js
+++ dist/js/anchor.js
@@ -1,9 +1,31 @@
-
+function onScroll () {
+    var scrollTop = $(document).scrollTop();
+    $('.header__nav-link').each(function(){
+      var hash = $(this).attr("href");
+      var target = $(hash);
+      if (target.position().top <= scrollTop && target.position().top + target.outerHeight() > scrollTop) {
+          $('.header__nav-link--active').removeClass("active");
+          $(this).addClass("header__nav-link--active");
+          } else {
+          $(this).removeClass("header__nav-link--active");
+          }
+          });
+  }
    $(document).ready(function(){
+    $(document).on("scroll", onScroll);
     $(".header__nav").on("click","a", function (event) {
         event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1000);
-    }); 
-}); 
+        $(document).off("scroll");
+        $('.header__nav-link--active').removeClass('header__nav-link--active')
+        $(this).addClass('header__nav-link--active');
+        var hash  = $(this).attr('href');
+        var target  = $(hash);
+        $("html, body").animate({
+            scrollTop: target.offset().top
+            }, 700, function(){
+            window.location.hash = hash;
+            $(document).on("scroll", onScroll);
+            });
+            });
+            });
+
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
+    $(window).scroll(function(){
+        if ($(window).scrollTop() > 200) {
+            $('.header').addClass('header--scrolled');
+            $('.header__inner').addClass('header__inner--scrolled');
+        }
+        else {
+            $('.header').removeClass('header--scrolled');
+            $('.header__inner').removeClass('header__inner--scrolled');
+        }});
+    
     $('.header__mob').click(function() {
         $('.header__burger').toggleClass('header__burger--active');
         $('.header__nav').toggleClass('header__nav--active');
