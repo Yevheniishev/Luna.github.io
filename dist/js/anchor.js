37ebea4a8626cf2bcf9fec3ab7cbb0a80c35f5e4
Yevhenii <yevhenii.shevchenko@ukr.net>
2019-02-01 15:06:09 +0200

    commit

--- dist/js/anchor.js
+++ dist/js/anchor.js
@@ -1,24 +1,29 @@
-$(document).scroll(function(){
-    var sections = $('section');
-sections.each(function(i,el){
-   var top  = $(el).offset().top - 100;
-   var bottom = top + $(el).height();
-   var scroll = $(document).scrollTop();
-   var id = $(el).attr('id');
-   if( scroll > top && scroll < bottom){
-       $('.header__nav-link--active').removeClass('header__nav-link--active');
-       $('a[href="#'+id+'"]').addClass('header__nav-link--active');
-
-   }
-})
-});
-
-$(".header__nav").on("click","a", function (event) {
-   event.preventDefault();
-   var id  = $(this).attr('href'),
-       top = $(id).offset().top;
-   $('body,html').animate({scrollTop: top - 50}, 800);
-});
-           
-            
+ $(document).ready(function () {
+        $(document).scroll(function(){
+            var sections = $('section');
+        sections.each(function(i,el){
+           var top  = $(el).offset().top - 100;
+           var bottom = top + $(el).height();
+           var scroll = $(document).scrollTop();
+           var id = $(el).attr('id');
+           if( scroll > top && scroll < bottom){
+               $('.header__nav-link--active').removeClass('header__nav-link--active');
+               $('a[href="#'+id+'"]').addClass('header__nav-link--active');
+        
+           }
+        })
+        });
+        
+        $(".header__nav").on("click","a", function (event) {
+           event.preventDefault();
+           var id  = $(this).attr('href');
+           var top = $(id).offset().top;
+           $('body,html').animate({scrollTop: top - 50}, 800);
+        });
+        $('.intro__rows').click(function () {
+            var nextSection = $('section').next();
+            var top = $(nextSection).offset().top;
+            $('body, html').animate({scrollTop: top - 50}, 800);
+        })
+    })        
 
