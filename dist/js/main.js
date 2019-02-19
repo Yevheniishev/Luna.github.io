d63e23d42b9f874a799a0c2cb6e2ecdb15605b64
Yevhenii <yevhenii.shevchenko@ukr.net>
2019-02-19 19:16:56 +0200

    commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
     $(window).scroll(function(){
-        if ($(window).scrollTop() > 200) {
+        if ($(window).scrollTop() > 50) {
             $('.header').addClass('header--scrolled');
             $('.header__inner').addClass('header__inner--scrolled');
-        }
+            if($('.header__burger').hasClass('header__burger--active')) {
+                $('.header').removeClass('header--scrolled');
+            }
+        } 
         else {
             $('.header').removeClass('header--scrolled');
             $('.header__inner').removeClass('header__inner--scrolled');
@@ -12,6 +15,12 @@ $(document).ready(function () {
     $('.header__mob').click(function() {
         $('.header__burger').toggleClass('header__burger--active');
         $('.header__nav').toggleClass('header__nav--active');
+        if($('.header__burger').hasClass('header__burger--active')) {
+            $('.header').removeClass('header--scrolled');
+        } else
+        if ($(window).scrollTop() > 50) {
+            $('.header').addClass('header--scrolled');
+        }
     })
 
     $('.slider').slick({
@@ -26,7 +35,7 @@ $(document).ready(function () {
                 breakpoint: 768,
                 settings: {
                     dots: true
-                }}]
+                }}]   
               });
   
       });  
